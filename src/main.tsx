74d08f3fdf82b6d876825fc57b6e09a931347fcf
Mariia Popelianets <mariakh0128@gmail.com>
2026-07-14 15:19:31 +0300

    feat: add movie search and modal

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 import "modern-normalize/modern-normalize.css";
 import App from "./components/App/App";
@@ -7,5 +8,6 @@ import App from "./components/App/App";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
 );
